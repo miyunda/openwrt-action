c35a6cef03d76040d823071f14242a2cb46c74f2
Yu from iMAC2019 <github@beijing2b.com>
2023-10-08 06:57:54 +0800

    Enable gzip images

--- custom1.sh
+++ custom1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (c) 2023 miyunda https://miyundacom>
-# OpenWrt custom script part 1 (Before Update feeds)
+# OpenWrt custom script part 1 (Before Updating feeds)
 
 # Introduce kenzo's repositories
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
